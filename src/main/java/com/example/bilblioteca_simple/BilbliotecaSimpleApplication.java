15da055d9a97c3aaa5ca33e06828e493e7cf0e5f
haruxvi <vicente.donoso004@gmail.com>
2025-04-17 09:36:58 -0400

    first commit

---
+++ src/main/java/com/example/bilblioteca_simple/BilbliotecaSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilblioteca_simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilbliotecaSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilbliotecaSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bilblioteca_simple/BilbliotecaSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilblioteca_simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilbliotecaSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
